cd0c6efc7582f0c59a4267fc2895aa69f9a48db8
ManavJoshi111 <73978411+ManavJoshi111@users.noreply.github.com>
2023-02-01 15:01:07 +0530

    Time: 0 ms (100.00%), Space: 13 MB (84.37%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int vis[n][m];
+        queue<pair<pair<int,int>,int>> q;
+        int ctrn=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
+                }
+                else
+                {
+                    vis[i][j]=0;
+                }
+                if(grid[i][j]==1)ctrn++;
+            }
+        }
+        int ctr=0;
+        int tm=0;
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        while(!q.empty())
+        {
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int t=q.front().second;
+            tm=max(tm,t);
+            q.pop();
+            for(int i=0;i<4;i++)
+            {
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and !vis[nrow][ncol] and grid[nrow][ncol]==1)
+                {
+                    q.push({{nrow,ncol},t+1});
+                    vis[nrow][ncol]=1;
+                    ctr++;
+                }
+            }
+        }
+        if(ctr==ctrn)
+            return tm;
+        return -1;
+    }
+};
